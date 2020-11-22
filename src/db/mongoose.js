0cbdf4b43fd152d245d07288d820415ce6f0df49
jun-y23 <ki8109smith@gmail.com>
2020-11-22 18:06:10 +0900

    refactor

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://ec2-54-150-50-94.ap-northeast-1.compute.amazonaws.com:27017', { 
+mongoose.connect('mongodb://10.0.2.10:27017', { 
     useCreateIndex: true,
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    ssl: true,
-    sslValidate: false,
-    // sslCA: fs.readFileSync(''),
-    user: 'admin',
-    pass: 'manager',
     dbName: "alversary"
 })
 .then(() => console.log('success!'))
